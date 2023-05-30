3a8dc318698b8da438624218a10592f22c1d529d
saikiran086 <saikirandulam1997@gmail.com>
2023-05-30 17:56:31 +0530

    welcome push

--- src/test/java/magneq/projectk/pages/RegisterPage.java
+++ src/test/java/magneq/projectk/pages/RegisterPage.java
@@ -2,6 +2,7 @@ package magneq.projectk.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import magneq.projectk.base.BaseClass;
 
@@ -11,17 +12,21 @@ public class RegisterPage extends BaseClass{
 		
 		this.driver= driver;
 		
+		
 	}
 	
 	By usernameLocator = By.xpath("//input[@name='username']");
 	By passwordLocator = By.xpath("//input[@name='password']");
 	By commentLocator=By.xpath("//textarea[@name='comments']");
 	
-	public void register(String username,String password,String comment) {
+	public void register(String username,String password,String comment) 
+	{
 		
 		driver.findElement(usernameLocator).sendKeys(username);
 		driver.findElement(passwordLocator).sendKeys(password);
 		driver.findElement(commentLocator).sendKeys(comment);
+		
+		System.out.println("hi git hub");
 		
 	}
 }
